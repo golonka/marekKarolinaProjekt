359c2e4cca4309f689be7943f2a55a747f8aa6a5
Karolina Florek <emertha@gmail.com>
2020-02-08 21:26:04 +0100

    dopis klasy

--- marekKarolinaProjekt/Specyfication.cs
+++ marekKarolinaProjekt/Specyfication.cs
@@ -2,7 +2,26 @@
 
 public class Specyfication
 {
-	public Specyfication()
-	{
-	}
+
+    private readonly String ram;
+    private readonly String graphic;
+    private readonly String processor;
+    private readonly String screen;
+
+    public Specyfication(String ram, String graphic, String processor, String screen)
+    {
+        this.ram = ram;
+        this.graphic = graphic;
+        this.processor = processor;
+        this.screen = screen;
+    }
+
+    public String Ram { get => ram; }
+
+    public String Graphic { get => graphic; }
+
+    public String Processor { get => processor; }
+
+    public String Screen { get => screen; }
+
 }
