f2a6bdf44170b7328923e84e981e6307d70b05b7
Karolina Florek <emertha@gmail.com>
2020-02-08 22:13:09 +0100

    update test

--- TestsKM/KarolinaTests.cs
+++ TestsKM/KarolinaTests.cs
@@ -34,5 +34,61 @@ namespace TestsKM
 
             NUnit.Framework.Assert.AreEqual(order.Amount, 7000);
         }
+
+
+        [Test]
+        public void Specyfication_Empty_String_Negative_Throws_Exception()
+        {
+            Assert.Throws<ArgumentException>(
+            () =>
+            {
+                new Specyfication(
+                   "","","",""
+                );
+            }
+            );
+        }
+
+        [Test]
+        public void Specyfication_Low_Standard_New_Order()
+        {
+            Specyfication lowStandard = new Specyfication(
+
+                "8",
+                "Nvidia",
+                "abc",
+                "dfe"
+            );
+
+            Order order = new Order
+            {
+                OrderId = 15,
+                ProductId = 500,
+                ProductQuantity = 1,
+                Amount = 500
+            };
+
+        }
+
+        [Test]
+        public void Specyfication_Medium_Standard_New_Order()
+        {
+            Specyfication lowStandard = new Specyfication(
+
+                "16",
+                "Nvidia",
+                "hhh",
+                "yyy"
+            );
+
+            Order order = new Order
+            {
+                OrderId = 17,
+                ProductId = 515,
+                ProductQuantity = 1,
+                Amount = 4000
+            };
+
+        }
     }
 }
