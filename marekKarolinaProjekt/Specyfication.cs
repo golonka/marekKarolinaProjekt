82e58192a6fb0718c83ad213417a7a0c3f2a5452
Karolina Florek <emertha@gmail.com>
2020-02-08 21:32:44 +0100

    klasa

--- marekKarolinaProjekt/Specyfication.cs
+++ marekKarolinaProjekt/Specyfication.cs
@@ -1,27 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Specyfication
+namespace marekKarolinaProjekt
 {
 
-    private readonly String ram;
-    private readonly String graphic;
-    private readonly String processor;
-    private readonly String screen;
-
-    public Specyfication(String ram, String graphic, String processor, String screen)
+    public class Specyfication
     {
-        this.ram = ram;
-        this.graphic = graphic;
-        this.processor = processor;
-        this.screen = screen;
+
+        private readonly String ram;
+        private readonly String graphic;
+        private readonly String processor;
+        private readonly String screen;
+
+        public Specyfication(String ram, String graphic, String processor, String screen)
+        {
+            this.ram = ram;
+            this.graphic = graphic;
+            this.processor = processor;
+            this.screen = screen;
+        }
+
+        public String Ram { get => ram; }
+
+        public String Graphic { get => graphic; }
+
+        public String Processor { get => processor; }
+
+        public String Screen { get => screen; }
+
     }
-
-    public String Ram { get => ram; }
-
-    public String Graphic { get => graphic; }
-
-    public String Processor { get => processor; }
-
-    public String Screen { get => screen; }
-
 }
